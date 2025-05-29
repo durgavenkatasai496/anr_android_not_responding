9426ce0300e68ce0cc07ad9de24cc468ebae1579
msai1 <medicherladurgasai@gamil.com>
2025-05-29 21:53:47 +0530

    Anr giving the commit

---
+++ app/src/main/java/com/example/anrchecking/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.anrchecking
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.anrchecking.ui.theme.ANRCheckingTheme
+
+/*
+ * 🧾 Definition of ANR:
+ANR (Application Not Responding) is a system error that happens when your app's main thread (UI thread) is too busy and doesn’t respond to the user in time.
+
+When the app doesn’t respond to touch, clicks, or other actions for a few seconds, Android shows a dialog saying:
+
+❗ "App isn’t responding. Do you want to close it?"
+ */
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ANRCheckingTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ANRCheckingTheme {
+        Greeting("Android")
+    }
+}
